60a2399af73cc4c7cc4513be394b81aa7eda6575
Mando Escamilla <mando@mando.org>
2009-03-26 10:19:48 -0500

    initial commit of membership model.

---
+++ db/migrate/20090326151904_create_memberships.rb
@@ -0,0 +1,12 @@
+class CreateMemberships < ActiveRecord::Migration
+  def self.up
+    create_table :memberships do |t|
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :memberships
+  end
+end
